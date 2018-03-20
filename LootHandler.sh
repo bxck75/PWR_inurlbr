fc507c690885198a495116737acc69603506c39e
bxck75 <goldenkooy@gmail.com>
2018-03-20 21:12:06 +0100

    loothandler added to K00B404.conf file (16)

--- LootHandler.sh
+++ LootHandler.sh
@@ -6,21 +6,22 @@ echo $line >> resultloothandler.tmp
 done < <(sqlite3 -separator $',' $DBPATH "SELECT ip,ports FROM BigDump where ports != ''") 
 
 
-	for p in $(cat resultloothandler.tmp); do
-		ip=`echo "$p" |awk -F, '{print $1}'`
-		port=`echo "$p" |awk -F, '{print $2}'`
-		#echo $ip":"$port
-		#echo $port
-		OLDIFS=$IFS
-		IFS=:
-		while read p
-		do
-			echo $p
-		done < $port
-		IFS=$OLDIFS
-		exit
-		for i in $(echo $port|awk -F':' '{print }'); do
-			echo ${ip}":"${i} >> outfile.tmp
-		done
+for p in $(cat resultloothandler.tmp); do
+	ip=`echo "$p" |awk -F, '{print $1}'`
+	port=`echo "$p" |awk -F, '{print $2}'`
+	#echo $ip":"$port
+	#echo $port
+	OLDIFS=$IFS
+	IFS=:
+	while read p
+	do
+		echo $p
+	done < $port
+	IFS=$OLDIFS
+	exit
+	for i in $(echo $port|awk -F':' '{print }'); do
+		echo ${ip}":"${i} >> outfile.tmp
 	done
-	cat outfile.tmp |sort -u
+done
+cat outfile.tmp |sort -u
+rm resultloothandler.tmp
